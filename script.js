ba84b00737ecfc5a2840f12476b609fcce6589cc
hutbni78 <tnw.w06@gmail.com>
2024-06-25 19:57:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ document.addEventListener("DOMContentLoaded", function() {
 function submitForm(event) {
     event.preventDefault(); // Mencegah pengiriman formulir secara default
 
+    var nama = document.getElementById('nama').value.trim();
+    var npp = document.getElementById('npp').value.trim();
+    var unit = document.getElementById('unit').value.trim();
+
+    // Validasi input kosong
+    if (nama === '' || npp === '' || unit === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Gagal',
+            text: 'Harap lengkapi semua kolom sebelum mengirim data.',
+            confirmButtonText: 'Tutup'
+        });
+        return; // Menghentikan fungsi submitForm jika ada input yang kosong
+    }
+
     var formData = new FormData(document.getElementById('formData'));
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://script.google.com/macros/s/AKfycbyXMDevfNduNoE27ZZaoPSkjbcp2F7QbAvGd8NhQrwGdWRvjStTiYDm_09CjcsJ098sew/exec'); // Ganti dengan URL dari Google Apps Script
@@ -46,7 +61,8 @@ function submitForm(event) {
     };
 }
 
-function disableForm() {
-    document.getElementById('formData').style.display = 'none';
-    document.getElementById('alreadySubmittedMessage').style.display = 'block';
-}
+
+// function disableForm() {
+//     document.getElementById('formData').style.display = 'none';
+//     document.getElementById('alreadySubmittedMessage').style.display = 'block';
+// }
