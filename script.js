31b626816ad976ebfd722260de7bf56535cecc2f
kesayanganmu <yudha.kkv@gmail.com>
2024-06-26 10:13:55 +0700

    Pesan commit

--- script.js
+++ script.js
@@ -11,8 +11,40 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const music = document.getElementById('backgroundMusic');
+    music.play().catch(() => {
+        // Pemutaran otomatis mungkin diblokir. Tambahkan pesan atau tombol untuk memulai musik
+        let playButton = document.createElement('button');
+        playButton.textContent = 'Klik untuk memutar musik';
+        playButton.style.position = 'absolute';
+        playButton.style.top = '10px';
+        playButton.style.left = '10px';
+        playButton.addEventListener('click', () => {
+            music.play();
+            playButton.remove();
+        });
+        document.body.appendChild(playButton);
+    });
+});
+
 function submitForm(event) {
     event.preventDefault(); // Mencegah pengiriman formulir secara default
+
+    var nama = document.getElementById('nama').value.trim();
+    var npp = document.getElementById('npp').value.trim();
+    var unit = document.getElementById('unit').value.trim();
+
+    // Validasi input kosong
+    if (nama === '' || npp === '' || unit === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Gagal',
+            text: 'Harap lengkapi semua kolom sebelum mengirim data.',
+            confirmButtonText: 'Tutup'
+        });
+        return; // Menghentikan fungsi submitForm jika ada input yang kosong
+    }
 
     var formData = new FormData(document.getElementById('formData'));
     var xhr = new XMLHttpRequest();
@@ -46,7 +78,8 @@ function submitForm(event) {
     };
 }
 
-function disableForm() {
-    document.getElementById('formData').style.display = 'none';
-    document.getElementById('alreadySubmittedMessage').style.display = 'block';
-}
+
+// function disableForm() {
+//     document.getElementById('formData').style.display = 'none';
+//     document.getElementById('alreadySubmittedMessage').style.display = 'block';
+// }
