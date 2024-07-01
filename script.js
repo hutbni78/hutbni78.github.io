76a69d39a7024501ca41e0bb5aa6b9a8b1d31248
hutbni78 <tnw.w06@gmail.com>
2024-07-01 09:00:53 +0700

    update 1 juli script.js

--- script.js
+++ script.js
@@ -15,58 +15,52 @@ document.addEventListener('DOMContentLoaded', function() {
     // Menampilkan pesan Selamat Datang
     let welcomeMessage = document.createElement('div');
     welcomeMessage.innerHTML = '<p>Selamat datang di acara HUT BNI ke-78 Kantor Wilayah Surabaya</p>';
-    // welcomeMessage.innerHTML = '<h3></h3>';
     welcomeMessage.style.textAlign = 'center';
     welcomeMessage.style.marginTop = '20px';
-    welcomeMessage.style.backgroundColor = 'rgba(255, 255, 255, 0.5)'; // Warna latar belakang transparan
+    welcomeMessage.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
     welcomeMessage.style.padding = '20px';
     welcomeMessage.style.fontSize = '20px';
     welcomeMessage.style.fontWeight = '800';
     welcomeMessage.style.borderRadius = '8px';
-    welcomeMessage.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)'; // Bayangan halus
-    welcomeMessage.style.maxWidth = '300px'; // Lebar maksimum sesuai kebutuhan
-    welcomeMessage.style.margin = '70px auto'; // Posisi di tengah horizontal
+    welcomeMessage.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    welcomeMessage.style.maxWidth = '300px';
+    welcomeMessage.style.margin = '70px auto';
     document.body.appendChild(welcomeMessage);
 
     const music = document.getElementById('backgroundMusic');
     music.play().catch(() => {
-        // Pemutaran otomatis mungkin diblokir. Tambahkan pesan atau tombol untuk memulai musik
         let playButton = document.createElement('button');
         playButton.textContent = 'Lanjut';
         playButton.style.position = 'absolute';
-        playButton.style.top = '50%'; // Menempatkan tombol di tengah vertikal
-        playButton.style.left = '50%'; // Menempatkan tombol di tengah horizontal
-        playButton.style.transform = 'translate(-50%, -50%)'; // Membuat tombol berada di tengah layar
-        playButton.style.padding = '10px 20px'; // Padding tombol
-        playButton.style.fontFamily = 'Arial, sans-serif'; // Jenis font
-        playButton.style.fontSize = '16px'; // Ukuran font
-        playButton.style.backgroundColor = '#007bff'; // Warna latar belakang
-        playButton.style.color = 'white'; // Warna teks
-        playButton.style.border = 'none'; // Tanpa border
-        playButton.style.borderRadius = '4px'; // Radius sudut
-        playButton.style.cursor = 'pointer'; // Kursor pointer
-        playButton.style.transition = 'background-color 0.3s ease'; // Animasi perubahan warna latar belakang
+        playButton.style.top = '50%';
+        playButton.style.left = '50%';
+        playButton.style.transform = 'translate(-50%, -50%)';
+        playButton.style.padding = '10px 20px';
+        playButton.style.fontFamily = 'Arial, sans-serif';
+        playButton.style.fontSize = '16px';
+        playButton.style.backgroundColor = '#007bff';
+        playButton.style.color = 'white';
+        playButton.style.border = 'none';
+        playButton.style.borderRadius = '4px';
+        playButton.style.cursor = 'pointer';
+        playButton.style.transition = 'background-color 0.3s ease';
     
-        // Efek hover
         playButton.addEventListener('mouseover', function() {
-            playButton.style.backgroundColor = '#0073ee'; // Warna latar belakang saat dihover
-            playButton.style.color = 'white'; // Warna teks saat dihover
+            playButton.style.backgroundColor = '#0073ee';
         });
     
         playButton.addEventListener('mouseout', function() {
-            playButton.style.backgroundColor = '#007bff'; // Kembali ke warna latar belakang awal saat tidak dihover
-            playButton.style.color = 'white'; // Kembali ke warna teks awal saat tidak dihover
+            playButton.style.backgroundColor = '#007bff';
         });
     
         playButton.addEventListener('click', () => {
             music.play();
             playButton.remove();
-            document.getElementById('formContainer').style.display = 'block'; // Menampilkan form setelah klik
-            welcomeMessage.remove(); // Menghapus pesan Selamat Datang setelah tombol diklik
+            document.getElementById('formContainer').style.display = 'block';
+            welcomeMessage.remove();
         });
         document.body.appendChild(playButton);
     });
-    
 });
 
 function submitForm(event) {
