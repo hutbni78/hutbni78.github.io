abcecd255402fd728f949631ff0b37ecdeb7a7b7
hutbni78 <tnw.w06@gmail.com>
2024-07-01 15:22:38 +0700

    new update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("hasSubmittedForm")) {
         disableForm();
-        // Menampilkan SweetAlert
         Swal.fire({
             icon: 'info',
             title: 'Informasi',
@@ -12,54 +11,24 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Menampilkan pesan Selamat Datang
     let welcomeMessage = document.createElement('div');
+    welcomeMessage.className = 'welcome-message';
     welcomeMessage.innerHTML = '<p>Selamat datang di acara HUT BNI ke-78 Kantor Wilayah Surabaya</p>';
-    welcomeMessage.style.textAlign = 'center';
-    welcomeMessage.style.marginTop = '20px';
-    welcomeMessage.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-    welcomeMessage.style.padding = '20px';
-    welcomeMessage.style.fontSize = '20px';
-    welcomeMessage.style.fontWeight = '800';
-    welcomeMessage.style.borderRadius = '8px';
-    welcomeMessage.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
-    welcomeMessage.style.maxWidth = '300px';
-    welcomeMessage.style.margin = '70px auto';
     document.body.appendChild(welcomeMessage);
 
     const music = document.getElementById('backgroundMusic');
     music.play().catch(() => {
-    let playButton = document.createElement('button');
-    playButton.textContent = 'Lanjut';
-    playButton.style.position = 'absolute';
-    playButton.style.top = '50%';
-    playButton.style.left = '50%';
-    playButton.style.transform = 'translate(-50%, -50%)';
-    playButton.style.padding = '10px 20px';
-    playButton.style.fontFamily = 'Arial, sans-serif';
-    playButton.style.fontSize = '16px';
-    playButton.style.backgroundColor = '#007bff';
-    playButton.style.color = 'white';
-    playButton.style.border = 'none';
-    playButton.style.borderRadius = '4px';
-    playButton.style.cursor = 'pointer';
-    playButton.style.transition = 'background-color 0.3s ease';
-    
-    playButton.addEventListener('mouseover', function() {
-        playButton.style.backgroundColor = '#0073ee';
-    });
-    
-    playButton.addEventListener('mouseout', function() {
-        playButton.style.backgroundColor = '#007bff';
-    });
-    
-    playButton.addEventListener('click', () => {
-        music.play();
-        playButton.remove();
-        document.getElementById('formContainer').style.display = 'block';
-        welcomeMessage.remove(); // Menghapus pesan Selamat Datang setelah tombol diklik
-    });
-        document.body.appendChild(playButton);
+        let playButton = document.createElement('button');
+        playButton.className = 'play-button';
+        playButton.textContent = 'Lanjut';
+        
+        playButton.addEventListener('click', () => {
+            music.play();
+            playButton.remove();
+            document.getElementById('formContainer').style.display = 'block';
+            welcomeMessage.remove();
+        });
+        welcomeMessage.appendChild(playButton);
     });
 });
 
